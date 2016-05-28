8cf968fbf7cec29f9af83b611295f97eba9fda49
RARvolt <rarvolt@gmail.com>
2016-05-29 01:05:01 +0200

    Create Settings Activity

--- app/src/main/java/net/rarvolt/fuelstats/MainActivity.java
+++ app/src/main/java/net/rarvolt/fuelstats/MainActivity.java
@@ -44,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         } else if (id == R.id.action_history) {
             startActivity(new Intent(this, RefuelingHistoryActivity.class));
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
